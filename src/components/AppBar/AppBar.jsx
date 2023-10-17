98c091d18a63613e84f68e799bdcadd9d3f13135
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-10-18 00:28:38 +0300

    1

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,14 +3,27 @@ import { UserMenu } from 'components/UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks/useAuth';
 import css from './AppBar.module.css';
+import { AppBar, Box, Icon,  Toolbar, Typography } from '@mui/material';
+import { Phone } from '@mui/icons-material';
 
 export const AppBarComp = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <header className={css.header}>
+      <AppBar position="static">
+        <Toolbar>
+          
+        <Icon sx={{mr:1}}>
+            <Phone/>
+          </Icon>
+          <Typography  >PhoneBook</Typography>
+          
+          
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </Toolbar>
+      </AppBar>
     </header>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
 import css from './Navigation.module.css';
+import { Box } from '@mui/material';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <nav>
+    <nav><Box sx={{ display: 'flex'}}>
       <NavLink className={css.link} to="/">
         Home
       </NavLink>
@@ -14,7 +15,7 @@ export const Navigation = () => {
         <NavLink className={css.link} to="/tasks">
           Tasks
         </NavLink>
-      )}
+      )}</Box>
     </nav>
   );
 };
