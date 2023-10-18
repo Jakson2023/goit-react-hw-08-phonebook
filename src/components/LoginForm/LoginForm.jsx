a5ea6a4beb5688971a52724584e651f5da5c7cd0
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-10-18 23:42:57 +0300

    login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import css from './LoginForm.module.css';
+import { Box, Button, TextField } from '@mui/material';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
@@ -18,16 +19,27 @@ export const LoginForm = () => {
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
+    <form  onSubmit={handleSubmit} autoComplete="off">
+      
+        <Box sx={{
+          display:'flex',
+          flexDirection:'column',
+          ml: 'auto',
+          mr: 'auto',
+          width:350,
+          mb:16
+        }}>
+        <TextField type="email" name="email" variant='outlined'
+             color='primary'label='Email' />
+      <Box height={14} />
+      
+        <TextField  variant='outlined'
+             color='primary'label='Password' type="password" name="password" />
+      <Box height={14} />
+      <Button type="submit" variant='contained'
+             color='primary'
+             size='large'>Log In</Button>
+             </Box>
     </form>
   );
 };
